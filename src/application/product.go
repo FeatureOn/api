b3024ae6fc402b574c063b96b84d482f78c0b66a
Serdar Kalaycı <serdar.kalayci@d-teknoloji.com.tr>
2020-12-12 18:42:06 +0300

    Added environment methods to product app layer

--- src/application/product.go
+++ src/application/product.go
@@ -9,15 +9,20 @@ import (
 
 // ProductRepository is the interface to interact with Product domain object
 type ProductRepository interface {
-	CheckProductNameAvailability(productID string, productName string) (bool, error)
-	AddProduct(productName string) error
+	GetProductByName(productName string) (string, error)
+	AddProduct(productName string) (string, error)
 	UpdateProduct(productID string, productName string) error
 	GetProducts() ([]domain.Product, error)
 	GetProduct(id string) (domain.Product, error)
-	CheckEnvironmentName(productID string, envirionmentName string) (bool, error)
-	AddEnvironment(productID string, environmentName string) error
-	AddFeature(productID string, feat domain.Feature)
-	UpdateFeature(productID string, feat domain.Feature)
+	GetEnvironmentByName(productID string, envirionmentName string) (string, error)
+	AddEnvironment(productID string, environmentName string) (string, error)
+	Updatenvironment(productID string, environmentID string, environmentName string) error
+	GetEnvironments(productID string) ([]domain.Environment, error)
+	GetEnvironment(productID string, environmentID string) (domain.Environment, error)
+	GetFeatureByName(productID string, featureName string) (string, error)
+	GetFeatureByKey(productID string, featureKey string) (string, error)
+	AddFeature(productID string, feat domain.Feature) (string, error)
+	UpdateFeature(productID string, feat domain.Feature) error
 	UpdateFeatureValue(productID string, environmentID string, featureID string, value bool)
 	DisableFeature(productID string, feat domain.Feature)
 	GetValues(productID string, environmentID string)
@@ -40,16 +45,30 @@ func NewProductService(pr ProductRepository) ProductService {
 
 // AddProduct first checks the availability of the name because the system should not allow the same name used twice
 // if the name is unique, it adds a new product to the repository injected into ProductService
-func (ps ProductService) AddProduct(productName string) error {
-	available, err := ps.productRepository.CheckProductNameAvailability("", productName)
+func (ps ProductService) AddProduct(productName string) (string, error) {
+	existingID, err := ps.productRepository.GetProductByName(productName)
+	if err != nil {
+		log.Error().Err(err).Msg("Error checking product name uniqueness")
+		return "", err
+	}
+	if existingID != "" {
+		return "", errors.New("The product name is not available")
+	}
+	return ps.productRepository.AddProduct(productName)
+}
+
+// UpdateProduct first checks the availability of the name because the system should not allow the same name used twice
+// if the name is unique, it adds a new product to the repository injected into ProductService
+func (ps ProductService) UpdateProduct(productID string, productName string) error {
+	existingID, err := ps.productRepository.GetProductByName(productName)
 	if err != nil {
 		log.Error().Err(err).Msg("Error checking product name uniqueness")
 		return err
 	}
-	if available != true {
+	if existingID != productID {
 		return errors.New("The product name is not available")
 	}
-	return ps.productRepository.AddProduct(productName)
+	return ps.productRepository.UpdateProduct(productID, productName)
 }
 
 // GetProducts returns a the list of products from the repository injected into ProductService
@@ -62,16 +81,61 @@ func (ps ProductService) GetProduct(id string) (domain.Product, error) {
 	return ps.productRepository.GetProduct(id)
 }
 
-// UpdateProduct first checks the availability of the name because the system should not allow the same name used twice
-// if the name is unique, it adds a new product to the repository injected into ProductService
-func (ps ProductService) UpdateProduct(productID string, productName string) error {
-	available, err := ps.productRepository.CheckProductNameAvailability(productID, productName)
+// AddEnvironment first checks the availability of the name because the system should not allow the same name used twice
+// if the name is unique, it adds a new environment on the product to the repository injected into ProductService
+func (ps ProductService) AddEnvironment(productID string, environmentName string) (string, error) {
+	existingID, err := ps.productRepository.GetEnvironmentByName(productID, environmentName)
 	if err != nil {
-		log.Error().Err(err).Msg("Error checking product name uniqueness")
+		log.Error().Err(err).Msg("Error checking environment name uniqueness")
+		return "", err
+	}
+	if existingID != productID {
+		return "", errors.New("The environment name is not available")
+	}
+	return ps.productRepository.AddEnvironment(productID, environmentName)
+}
+
+// UpdateEnvironment first checks the availability of the name because the system should not allow the same name used twice
+// if the name is unique, it updates the existing environment on the product to the repository injected into ProductService
+func (ps ProductService) UpdateEnvironment(productID string, environmentID string, environmentName string) error {
+	existingID, err := ps.productRepository.GetEnvironmentByName(productID, environmentName)
+	if err != nil {
+		log.Error().Err(err).Msg("Error checking environment name uniqueness")
 		return err
 	}
-	if available != true {
-		return errors.New("The product name is not available")
+	if existingID != environmentName {
+		return errors.New("The environment name is not available")
 	}
-	return ps.productRepository.UpdateProduct(productID, productName)
+	return ps.productRepository.Updatenvironment(productID, environmentID, environmentName)
+}
+
+// GetEnvironments returns a the list of environments defined for a product from the repository injected into ProductService
+func (ps ProductService) GetEnvironments(productID string) ([]domain.Environment, error) {
+	return ps.productRepository.GetEnvironments(productID)
+}
+
+// GetEnvironment returns an environment defined for a product from the repository injected into ProductService
+func (ps ProductService) GetEnvironment(productID string, environmentID string) (domain.Environment, error) {
+	return ps.productRepository.GetEnvironment(productID, environmentID)
+}
+
+// AddFeature first checks the uniqueness of Feature@s Name and Key because the system should not allow Name and Key used twice
+func (ps ProductService) AddFeature(productID string, feat domain.Feature) (string, error) {
+	existingID, err := ps.productRepository.GetFeatureByName(productID, feat.Name)
+	if err != nil {
+		log.Error().Err(err).Msg("Error checking feature name uniqueness")
+		return "", err
+	}
+	if existingID != "" {
+		return "", errors.New("The freature name is not available")
+	}
+	existingID, err = ps.productRepository.GetFeatureByKey(productID, feat.Key)
+	if err != nil {
+		log.Error().Err(err).Msg("Error checking feature key uniqueness")
+		return "", err
+	}
+	if existingID != "" {
+		return "", errors.New("The freature key is not available")
+	}
+	return ps.productRepository.AddFeature(productID, feat)
 }
