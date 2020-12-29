a9b48c7bc555b3fd8383b618b6b4e3bb560fa823
Serdar Kalaycı <serdarkalayci@gmail.com>
2020-12-29 16:32:35 +0300

    Update AddEnvrionment with transaction add environment and add flags inside a transaction

--- src/adapters/data/mongodb/environment.go
+++ src/adapters/data/mongodb/environment.go
@@ -12,14 +12,20 @@ import (
 	"github.com/spf13/viper"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/writeconcern"
 )
 
 // AddEnvironment adds a new environment together with all its flags with default values and returns its ID,
 // returns empty string and error otherwise
 func (pr ProductRepository) AddEnvironment(product domain.Product, environmentName string, environmentFlag domain.EnvironmentFlag) (string, error) {
 	productDAO := mappers.MapProduct2ProductDAO(product)
-	collection := pr.dbClient.Database(pr.dbName).Collection(viper.GetString("ProductsCollection"))
+	// Preperation for inter-collection transactions
+	wcMajority := writeconcern.New(writeconcern.WMajority(), writeconcern.WTimeout(1*time.Second))
+	wcMajorityCollectionOpts := options.Collection().SetWriteConcern(wcMajority)
+	productCollection := pr.dbClient.Database(pr.dbName).Collection(viper.GetString("ProductsCollection"), wcMajorityCollectionOpts)
+	flagCollection := pr.dbClient.Database(pr.dbName).Collection(viper.GetString("FlagsCollection"), wcMajorityCollectionOpts)
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 	newEnvID := primitive.NewObjectID()
@@ -28,27 +34,41 @@ func (pr ProductRepository) AddEnvironment(product domain.Product, environmentNa
 		Name: environmentName,
 	}
 	productDAO.Environments = append(productDAO.Environments, newEnv)
-	idDoc := bson.D{{"_id", productDAO.ID}}
 
-	upDoc := bson.D{{"$set", bson.M{"environments": productDAO.Environments}}}
-	var updateOpts options.UpdateOptions
-	updateOpts.SetUpsert(false)
-	result, err := collection.UpdateOne(ctx, idDoc, upDoc, &updateOpts)
-	if err == nil {
-		if result.MatchedCount == 1 {
-			// Add the flags for the new environment
-			flagCollection := pr.dbClient.Database(pr.dbName).Collection(viper.GetString("FlagsCollection"))
-			environmentFlag.EnvironmentID = newEnvID.Hex()
-			_, err = flagCollection.InsertOne(ctx, mappers.MapEnvironmentFlag2EnvironmentFlagDAO(environmentFlag))
-			return newEnvID.Hex(), nil
-		} else {
-			log.Error().Err(err).Msgf("The productID %s did not match any products in the database", product.ID)
-			return "", errors.New("Product not found")
+	// Step 1: Define the callback that specifies the sequence of operations to perform inside the transaction.
+	callback := func(sessCtx mongo.SessionContext) (interface{}, error) {
+		// Important: You must pass sessCtx as the Context parameter to the operations for them to be executed in the
+		// transaction.
+		idDoc := bson.D{{"_id", productDAO.ID}}
+		upDoc := bson.D{{"$set", bson.M{"environments": productDAO.Environments}}}
+		var updateOpts options.UpdateOptions
+		updateOpts.SetUpsert(false)
+
+		if _, err := productCollection.UpdateOne(sessCtx, idDoc, upDoc, &updateOpts); err != nil {
+			return nil, err
 		}
-	} else {
+		environmentFlag.EnvironmentID = newEnvID.Hex()
+
+		if _, err := flagCollection.InsertOne(sessCtx, mappers.MapEnvironmentFlag2EnvironmentFlagDAO(environmentFlag)); err != nil {
+			return nil, err
+		}
+
+		return nil, nil
+	}
+	// Step 2: Start a session and run the callback using WithTransaction.
+	session, err := pr.dbClient.StartSession()
+	if err != nil {
+		panic(err)
+	}
+	defer session.EndSession(ctx)
+
+	result, err := session.WithTransaction(ctx, callback)
+	if err != nil {
 		log.Error().Err(err).Msgf("Error adding environment with name %s", environmentName)
 		return "", err
 	}
+	log.Info().Msgf("result: %v\n", result)
+	return "ok", nil
 }
 
 // UpdateEnvironment updates an existing environment on the database
