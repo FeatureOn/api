7435154e8beb90524de637585eea11ff3de4800e
Serdar Kalaycı <serdarkalayci@gmail.com>
2021-01-11 13:46:03 +0300

    Added missing comments

--- src/application/health.go
+++ src/application/health.go
@@ -5,10 +5,12 @@ type HealthRepository interface {
 	Ready() bool
 }
 
+// HealthService is the struct to let outer layers to interact to the health applicatopn
 type HealthService struct {
 	HealthRepository HealthRepository
 }
 
+// NewHealthService creates a new HealthService instance and sets its repository
 func NewHealthService(hr HealthRepository) HealthService {
 	if hr == nil {
 		panic("missing HealthRepository")
@@ -18,6 +20,7 @@ func NewHealthService(hr HealthRepository) HealthService {
 	}
 }
 
+// Ready returns true if underlying reposiroty and its connection is up and running, false otherwise
 func (hs HealthService) Ready() bool {
 	return hs.HealthRepository.Ready()
 }
