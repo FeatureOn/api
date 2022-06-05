5156124a6d0fca8f73c5af9bcf46e31258e63a4f
Serdar Kalaycı <serdarkalayci@gmail.com>
2022-06-05 16:53:15 +0200

    Implemented methods for environments on cockroach. Fixes #4

--- server/adapters/data/cockroachdb/product.go
+++ server/adapters/data/cockroachdb/product.go
@@ -114,13 +114,49 @@ func (pr ProductRepository) GetProduct(id string) (domain.Product, error) {
 }
 
 func (pr ProductRepository) AddEnvironment(product domain.Product, environmentName string, environmentFlag domain.EnvironmentFlag) (string, error) {
-	//TODO implement me
-	panic("implement me")
+	id := uuid.New()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	tran, err := pr.cp.Begin(ctx)
+	if err != nil {
+		return "", err
+	}
+	result, err := pr.cp.Exec(ctx, fmt.Sprintf("insert into %s.environments (ID, Name, ProductID) values ($1, $2, $3)", pr.dbName), id, environmentName, product.ID)
+	if err != nil {
+		tran.Rollback(ctx)
+		return "", err
+	}
+	if result.RowsAffected() != 1 {
+		tran.Rollback(ctx)
+		return "", errors.New("Error adding the environment")
+	}
+	// We have to add flags for this environment for each feature
+	for _, flag := range environmentFlag.Flags {
+		result, err = pr.cp.Exec(ctx, fmt.Sprintf("insert into %s.flags (FeatureKey, EnvironmentID, Value) values ($1, $2, $3)", pr.dbName), flag.FeatureKey, id, flag.Value)
+		if err != nil {
+			tran.Rollback(ctx)
+			return "", err
+		}
+		if result.RowsAffected() != 1 {
+			tran.Rollback(ctx)
+			return "", errors.New("Error adding the environment")
+		}
+	}
+	tran.Commit(ctx)
+	return id.String(), nil
 }
 
 func (pr ProductRepository) UpdateEnvironment(product domain.Product, environmentID string, environmentName string) error {
-	//TODO implement me
-	panic("implement me")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	result, err := pr.cp.Exec(ctx, fmt.Sprintf("update %s.environments set Name=$1 where ID=$2", pr.dbName), environmentName, environmentID)
+	if err != nil {
+		return err
+	}
+	if result.RowsAffected() != 1 {
+		return errors.New("Error updating the environment")
+	}
+	return nil
 }
 
 func (pr ProductRepository) AddFeature(product domain.Product, feature domain.Feature, envFlags []domain.EnvironmentFlag) error {
