fca940c87abf627535e18e0e3a103e80040cb5d2
Serdar Kalaycı <serdarkalayci@gmail.com>
2022-06-05 23:43:00 +0200

    Implementted flag methods for cockroach. Fixes #6

--- server/adapters/data/cockroachdb/flag.go
+++ server/adapters/data/cockroachdb/flag.go
@@ -1,8 +1,12 @@
 package cockroachdb
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"github.com/FeatureOn/api/server/domain"
 	"github.com/jackc/pgx/v4/pgxpool"
+	"time"
 )
 
 type FlagRepository struct {
@@ -17,12 +21,36 @@ func newFlagRepository(pool *pgxpool.Pool, databaseName string) FlagRepository {
 	}
 }
 
-func (f FlagRepository) GetFlags(environmentID string) (domain.EnvironmentFlag, error) {
-	//TODO implement me
-	panic("implement me")
+// GetFlags gets values of all active flags for a given environment
+func (fr FlagRepository) GetFlags(environmentID string) (domain.EnvironmentFlag, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	rows, err := fr.cp.Query(ctx, fmt.Sprintf("select FeatureKey, Value from %s.flags where EnvironmentID=$1", fr.dbName), environmentID)
+	defer rows.Close()
+	envFlag := domain.EnvironmentFlag{EnvironmentID: environmentID}
+	if err != nil {
+		return domain.EnvironmentFlag{}, err
+	}
+	for rows.Next() {
+		var flag domain.Flag
+		if err = rows.Scan(&flag.FeatureKey, &flag.Value); err != nil {
+			return domain.EnvironmentFlag{}, err
+		}
+		envFlag.Flags = append(envFlag.Flags, flag)
+	}
+	return envFlag, nil
 }
 
-func (f FlagRepository) UpdateFlag(environmentID string, featureKey string, value bool) error {
-	//TODO implement me
-	panic("implement me")
+// UpdateFlag sets new value to a specific flag
+func (fr FlagRepository) UpdateFlag(environmentID string, featureKey string, value bool) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	result, err := fr.cp.Exec(ctx, fmt.Sprintf("update %s.flags set Value=$1 where FeatureKey=$2 and EnvironmentID=$3", fr.dbName), value, featureKey, environmentID)
+	if err != nil {
+		return err
+	}
+	if result.RowsAffected() != 1 {
+		return errors.New("error updating the flag")
+	}
+	return nil
 }
