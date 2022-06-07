d44708b9cb7cac576af888226962d2f4b23e6356
Serdar Kalaycı <serdarkalayci@gmail.com>
2022-06-07 11:50:29 +0200

    Added unit test for feature updates #10

--- server/application/feature_test.go
+++ server/application/feature_test.go
@@ -50,3 +50,48 @@ func TestAddFeature(t *testing.T) {
 	err = ps.AddFeature(mockProduct.ID, mockFeature)
 	assert.Nil(t, err)
 }
+
+func TestUpdateFeature(t *testing.T) {
+	ps := NewProductService(mockProductRepository{}, mockFlagRepository{})
+	// Check what happens when underlying database provider returns an error while checking the product
+	getProductFunc = func(id string) (domain.Product, error) {
+		return domain.Product{}, errors.New("some kind of database provider error while getting the product")
+	}
+	mockFeature := mocker.GenerateMockFeature()
+	err := ps.UpdateFeature("abc-def", mockFeature)
+	assert.EqualError(t, err, "some kind of database provider error while getting the product")
+	// Check what happens when we try to update an existing feature with an existing name
+	mockProduct := mocker.GenerateMockProduct(true, true)
+	// Let's ensure that out mock product has more than one feature
+	for len(mockProduct.Features) < 2 {
+		mockProduct = mocker.GenerateMockProduct(true, true)
+	}
+	getProductFunc = func(id string) (domain.Product, error) {
+		return mockProduct, nil
+	}
+	mockFeature.Key = mockProduct.Features[0].Key
+	mockFeature.Name = mockProduct.Features[1].Name
+	err = ps.UpdateFeature(mockProduct.ID, mockFeature)
+	assert.EqualError(t, err, "the feature name is not available")
+	// Check what happens when we try to update a feature with non-existing key
+	mockFeature = mocker.GenerateMockFeature()
+	getProductFunc = func(id string) (domain.Product, error) {
+		return mockProduct, nil
+	}
+	err = ps.UpdateFeature(mockProduct.ID, mockFeature)
+	assert.EqualError(t, err, "the feature key could not be found")
+	// Check when everything is OK but an error returned when updating the feature
+	mockFeature = mocker.GenerateMockFeature()
+	updateFeatureFunc = func(product domain.Product, feature domain.Feature) error {
+		return errors.New("feature updating error")
+	}
+	mockFeature.Key = mockProduct.Features[0].Key
+	err = ps.UpdateFeature(mockProduct.ID, mockFeature)
+	assert.EqualError(t, err, "feature updating error")
+	// Check when everything is OK and feature is updated
+	updateFeatureFunc = func(product domain.Product, feature domain.Feature) error {
+		return nil
+	}
+	err = ps.UpdateFeature(mockProduct.ID, mockFeature)
+	assert.Nil(t, err)
+}
